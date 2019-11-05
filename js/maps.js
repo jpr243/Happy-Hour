284007e6dc63d34fd80f19b1c8ceb56515092e03
Maddie <madeleinecgribble@gmail.com>
2019-11-05 20:23:38 +0800

    Changed from Mapbox to Foursquare API. Query filters applied

--- js/maps.js
+++ js/maps.js
@@ -1,15 +1,14 @@
 function searchLocation(userInput) {
-  searchTerm = userInput;
   queryUrl =
-    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-    userInput +
-    ".json?types=poi&access_token=pk.eyJ1IjoibWFkZGllY2xhcmUiLCJhIjoiY2syaDVobmM3MDZlYzNob2h3dXZoZWM3MiJ9.OAX_vNLH05xN2n1612UXkQ";
+    "https://api.foursquare.com/v2/venues/search?near=" +
+    userInput+
+    "&categoryId=4bf58dd8d48988d116941735&radius=1000&client_id=JQIY4W3MHQQLTPORXPCA2XJDIWTFHBZDTLJPO4F3IBWLH5NI&client_secret=TOTL05GAHJ5IFFA44YJL1HNPLB2HDABTV025VKIRNN34WYYV&v=20191105";
 
   $.ajax({
     url: queryUrl,
     method: "GET"
   }).then(function(response) {
-    let results = response.features;
+    let results = response.response.venues;
     console.log(queryUrl);
     console.log(results);
   });
