2d7bb0894b3050af72ee0158e0ad9e6ecebe38bf
Maddie <madeleinecgribble@gmail.com>
2019-11-05 20:30:50 +0800

    Renamed maps.js to logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,7 @@
 function searchLocation(userInput) {
   queryUrl =
     "https://api.foursquare.com/v2/venues/search?near=" +
-    userInput+
+    userInput +
     "&categoryId=4bf58dd8d48988d116941735&radius=1000&client_id=JQIY4W3MHQQLTPORXPCA2XJDIWTFHBZDTLJPO4F3IBWLH5NI&client_secret=TOTL05GAHJ5IFFA44YJL1HNPLB2HDABTV025VKIRNN34WYYV&v=20191105";
 
   $.ajax({
