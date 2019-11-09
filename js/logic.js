b7c4fa56ac346e8eff7f20bb08f069ed9c30bb64
mrduncle1 <dun__can@hotmail.com>
2019-11-09 13:37:43 +0800

    merge changes in order to pull down latest changes from github

--- js/logic.js
+++ js/logic.js
@@ -16,7 +16,10 @@ function searchByUserLocation(userInput) {
   function showResults(response) {
     searchResults = response.response.venues;
     console.log(queryUrl);
+    let lat = response.response.venues[0].location.lat;
+    let lng = response.response.venues[0].location.lng;
     appendLocationDetailsToPage(searchResults);
+    getPhotos(lat, lng);
   }
 }
 
@@ -24,4 +27,17 @@ function appendLocationDetailsToPage(locations) {
   for (let i = 0; i < locations.length; i++) {
     console.log(locations[i].name);
   }
+}
+
+function getPhotos(lat, lng) {
+  let queryURL = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=" + suburb + "&cuisines=bar%2C%20pub%2C%20nightlife";
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        headers: { "user-key": "7c5b101b634a31bcfcda3cf933e803ba" }
+    }).then(function(response){
+        ajaxResponse = response;
+        console.log(response);
+        showPhotos();
+    })
 }
